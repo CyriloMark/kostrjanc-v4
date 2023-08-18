ae4a931cfe7492f6b9b620eaf4d1eee40204c1bd
Cyril Mark <cyrilo.mark@gmail.com>
2023-08-18 13:35:13 +0200

    Updated Icons

--- assets/svg/Ban.js
+++ assets/svg/Ban.js
@@ -1,13 +1,28 @@
-import Svg, { Path, Rect } from "react-native-svg"
+import Svg, { Path, Rect } from "react-native-svg";
 
 export default function Ban(props) {
-
-    const banPath = "M238.14,246.24l-32.54-32.54-32.08,32.08-35.28,35.28L13.15,406.15c-17.71,17.71-17.51,46.64,.46,64.62l16.27,16.27c17.97,17.97,46.9,18.18,64.62,.46l125.09-125.09,35.28-35.28,32.08-32.08-32.54-32.54-16.27-16.27Z";
-
     return (
-        <Svg style={props.style} viewBox="0 0 500 500">
-            <Rect x="229" y="-28" width="138" height="460" rx="23" ry="23" transform="translate(-55.50 270) rotate(-45)" fill={props.fill}/>
-            <Path fill={props.fill} d={banPath} />
+        <Svg
+            style={props.style}
+            viewBox="0 0 481.41 482.41"
+            stroke={props.fill}
+            strokeWidth={2}>
+            <Rect
+                x="5"
+                y="310"
+                rx={"25"}
+                ry={"25"}
+                width="252.5"
+                height="101"
+                transform="translate(-214.75 193.66) rotate(-45)"
+                fill={props.fill}
+            />
+            <Path
+                fill={props.fill}
+                d={
+                    "m474.46,265.68L215.73,6.95c-7.94-7.94-20.81-7.94-28.75,0l-100.62,100.62c-7.94,7.94-7.94,20.81,0,28.75l258.73,258.73c7.94,7.94,20.81,7.94,28.75,0l100.62-100.62c7.94-7.94,7.94-20.81,0-28.75Z"
+                }
+            />
         </Svg>
-    )
+    );
 }
--- assets/svg/Pencil.js
+++ assets/svg/Pencil.js
@@ -1,26 +1,36 @@
-import Svg, { Rect, Polygon } from "react-native-svg";
+import Svg, { Rect, Polygon, Path } from "react-native-svg";
 
 export default function Pencil(props) {
     return (
-        <Svg style={props.style} viewBox="0 0 500 500" fill={"transparent"}>
-            <Rect
-                x="125"
-                y="100"
-                width="450"
-                height="195"
-                rx="52"
-                ry="52"
-                transform="translate(0 270) rotate(-45) scale(.8)"
-                strokeWidth={25}
-                stroke={props.fill}
+        // <Svg style={props.style} viewBox="0 0 500 500" fill={"transparent"}>
+        //     <Rect
+        //         x="125"
+        //         y="100"
+        //         width="450"
+        //         height="195"
+        //         rx="52"
+        //         ry="52"
+        //         transform="translate(0 270) rotate(-45) scale(.8)"
+        //         strokeWidth={25}
+        //         stroke={props.fill}
+        //     />
+        //     <Polygon
+        //         x="50"
+        //         y="50"
+        //         transform="scale(.8)"
+        //         points="59.06 304.64 29.53 402.32 0 500 97.68 470.47 195.36 440.94 127.21 372.79 59.06 304.64"
+        //         stroke={props.fill}
+        //         strokeWidth={25}
+        //     />
+        // </Svg>
+        <Svg style={props.style} viewBox="0 0 386.21 385.98">
+            <Path
+                d="m300.85,15c9.35,0,18.14,3.64,24.75,10.25l35.36,35.36c6.61,6.61,10.25,15.4,10.25,24.75s-3.64,18.14-10.25,24.75l-201.53,201.53-84.85-84.85L276.1,25.25c6.61-6.61,15.4-10.25,24.75-10.25m0-15c-12.8,0-25.59,4.88-35.36,14.64L53.36,226.78l106.07,106.07,212.13-212.13c19.53-19.53,19.53-51.18,0-70.71l-35.36-35.36c-9.76-9.76-22.56-14.64-35.36-14.64h0Z"
+                fill={props.fill}
             />
             <Polygon
-                x="50"
-                y="50"
-                transform="scale(.8)"
-                points="59.06 304.64 29.53 402.32 0 500 97.68 470.47 195.36 440.94 127.21 372.79 59.06 304.64"
-                stroke={props.fill}
-                strokeWidth={25}
+                points="28.28 357.69 67.18 241.02 53.03 226.88 0 385.98 159.1 332.94 144.96 318.8 28.28 357.69"
+                fill={props.fill}
             />
         </Svg>
     );
